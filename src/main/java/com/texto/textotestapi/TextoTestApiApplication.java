04b45f8f163928e90ddf8ccb271b2213ea802cfd
Bernardo Dantas <bernardo.cioglia@gmail.com>
2024-02-27 18:04:44 -0300

    SpringBoot project created.

---
+++ src/main/java/com/texto/textotestapi/TextoTestApiApplication.java
@@ -0,0 +1,13 @@
+package com.texto.textotestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextoTestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextoTestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/texto/textotestapi/TextoTestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.texto.textotestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextoTestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
