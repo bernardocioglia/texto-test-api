d9d752b8d9579d6b3f0d4dc021375322d94a24af
Bernardo Dantas <bernardo.cioglia@gmail.com>
2024-03-06 00:57:37 -0300

    Removing dead comments and adding rest call test.

--- src/test/java/com/texto/textotestapi/services/MovieServiceTests.java
+++ src/test/java/com/texto/textotestapi/services/MovieServiceTests.java
@@ -9,11 +9,13 @@ import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
 
 import com.texto.textotestapi.entities.Movie;
@@ -22,9 +24,11 @@ import com.texto.textotestapi.repositories.MovieRepository;
 
 @ActiveProfiles("test")
 @TestMethodOrder(OrderAnnotation.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class MovieServiceTests {
 
+	@LocalServerPort
+	private int port;
 	@Autowired
 	private MovieRepository repository;
 
@@ -32,8 +36,10 @@ public class MovieServiceTests {
 		// TODO Auto-generated constructor stub
 	}
 
+	@Autowired
+	private TestRestTemplate restTemplate;
+
 	@Test
-	@Order(value = 2)
 	public void loadMoviesCSV() {
 
 		final URL url = ClassLoader.getSystemResource("movielist.csv");
@@ -44,39 +50,8 @@ public class MovieServiceTests {
 		Assertions.assertDoesNotThrow(() -> this.repository.saveAll(csvMovies));
 		final List<Movie> dbMovies = Assertions.assertDoesNotThrow(() -> this.repository.findAll());
 		Assertions.assertEquals(dbMovies.size(), csvMovies.size());
+		org.assertj.core.api.Assertions.assertThat(this.restTemplate
+				.getForObject("http://localhost:" + this.port + "/movies", List.class).size() == csvMovies.size());
 
-//		URL url;
-//		url = ClassLoader.getSystemResource("movielist.csv");
-//		if (url == null) {
-//			// throw new RuntimeException("fail to find CSV file movielist.csv");
-//			Assertions.assertTrue(false, "fail to find CSV file movielist.csv");
-//		}
-//
-//		URI uri;
-//		uri = Assertions.assertDoesNotThrow(() -> url.toURI());
-//		try {
-//			uri = url.toURI();
-//		} catch (final URISyntaxException uriSyntaxException) {
-//			throw new RuntimeException("fail to convert CSV file URL into URI: " + uriSyntaxException.getMessage());
-//		}
-//
-//		InputStream inputStream;
-//		try {
-//			inputStream = Files.newInputStream(Paths.get(uri));
-//		} catch (final IOException ioException) {
-//			throw new RuntimeException("fail to open CSV file: " + ioException.getMessage());
-//		}
-//
-//		final List<Movie> csvMovies = CSVHelper.csvToMovies(inputStream);
-//
-//		try {
-//			this.repository.saveAll(csvMovies);
-//		} catch (final IllegalArgumentException illegalArgumentException) {
-//			throw new RuntimeException("fail to store CSV file: " + illegalArgumentException.getMessage());
-//		}
-
-//		final List<Movie> dbMovies = this.repository.findAll();
-//
-//		Assertions.assertEquals(dbMovies.size(), csvMovies.size());
 	}
 }
